4a8e67d947076c5741765b5b14985a2ed03c96b7
Azaz-Ahmad <azaztapadar4031@gmail.com>
2022-05-08 18:26:12 +0400

    added 3 new ml models

--- ml_model.py
+++ ml_model.py
@@ -1,1 +1,10 @@
 print("Adding all ml related coding here.")
+
+def model1():
+    print("Adding code for ML model1")
+
+def model2():
+    print("Adding code for ML model2")
+
+def model3():
+    print("Adding code for ML model3")
